d6f3cb0e927e437b783fc4e864accc1c62faddd3
harold jhan <haroldzhen531@gmail.com>
2021-08-08 20:51:11 +0800

    index.html & layout.ejs

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,2 +1,13 @@
-import '../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 console.log('Hello!');
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 15,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2000,
+  },
+});

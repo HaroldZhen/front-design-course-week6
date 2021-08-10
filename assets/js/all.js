2f5ed0ead935ceda60c339e013fd0083f739f273
harold jhan <haroldzhen531@gmail.com>
2021-08-10 17:53:19 +0800

    Update 2021-08-10T09:53:15.899Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -14,4 +14,78 @@ var swiper = new Swiper('.swiper-container', {
   // direction: 'vertical',
 
 });
+
+function favoriteShow() {
+  Swal.fire({
+    position: 'center',
+    icon: 'info',
+    title: '已收藏展覽',
+    showConfirmButton: false,
+    showCloseButton: true,
+    timer: 1500
+  });
+}
+
+function addTicket() {
+  var showName = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '展覽';
+  Swal.fire({
+    title: showName,
+    inputLabel: '你想購買幾張票？',
+    icon: 'question',
+    input: 'range',
+    inputAttributes: {
+      min: 1,
+      max: 10,
+      step: 1
+    },
+    inputValue: 1
+  });
+}
+
+function loginMsg() {
+  Swal.fire({
+    position: 'center',
+    icon: 'success',
+    title: '登入成功',
+    text: '模擬登入訊息',
+    showConfirmButton: false,
+    showCloseButton: true,
+    timer: 1500
+  });
+}
+
+function registerMsg() {
+  Swal.fire({
+    position: 'center',
+    icon: 'success',
+    title: '註冊成功',
+    text: '模擬註冊訊息',
+    showConfirmButton: false,
+    showCloseButton: true,
+    timer: 1500
+  });
+}
+
+function subscriptionMsg() {
+  Swal.fire({
+    position: 'center',
+    icon: 'success',
+    title: '感謝你的訂閱',
+    text: '我們將定期寄送活動資訊給你',
+    showConfirmButton: false,
+    showCloseButton: true,
+    timer: 1500
+  });
+}
+
+function addProductMsg() {
+  Swal.fire({
+    position: 'center',
+    icon: 'success',
+    title: '已新增到購物車',
+    showConfirmButton: false,
+    showCloseButton: true,
+    timer: 1500
+  });
+}
 //# sourceMappingURL=all.js.map
